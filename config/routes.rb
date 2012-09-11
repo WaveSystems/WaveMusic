5b096e68f23321ff2955403f28d430061ee05b69
Antonio Chavez <cavjzz@gmail.com>
2012-09-11 12:38:01 -0500

    Changed controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 WaveMusic::Application.routes.draw do
+  devise_for :users
+
   root to: 'pages#index'
 
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :registrations, :only => [:new, :create, :edit, :update]
+      resources :sessions, :only => [:new, :create, :edit, :update]
     end
   end
 end
