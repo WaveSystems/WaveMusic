d2479e2b312b333885f8c15c348deee4bcff42f7
Antonio Chavez <cavjzz@gmail.com>
2012-09-13 13:16:05 -0500

    Added first steps for register and login

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::RegistrationsController < ApplicationController
   respond_to :json, :xml
 
+  def create
+
+    user = User.new(params[:user])
+    if user.save
+      render :json=> user.as_json(:auth_token=>user.authentication_token, :email=>user.email), :status=>201
+      return
+    else
+      warden.custom_failure!
+      render :json=> user.errors, :status=>422
+    end
+  end
+
 end
